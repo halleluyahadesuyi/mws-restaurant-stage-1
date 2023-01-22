7d2722c0f1317ce75b0f8088f8cb57a8bb409112
“Halleluyah-A” <Halleluyah@acutro.co.uk>
2023-01-22 17:16:20 +0100

    Update app

--- js/main.js
+++ js/main.js
@@ -160,7 +160,9 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.tabIndex = 0;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " promotional image for restaurant";
   li.append(image);
 
   const name = document.createElement('h1');
@@ -169,10 +171,12 @@ createRestaurantHTML = (restaurant) => {
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.tabIndex = 0;
   li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
+  address.tabIndex = 0;
   li.append(address);
 
   const more = document.createElement('a');
